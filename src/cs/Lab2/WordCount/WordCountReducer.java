641ae4a0b3fd8243b3b5f7fd18ce486141b4db73
Valentin GRASSET <33599781+pnmoins2@users.noreply.github.com>
2018-01-09 00:09:58 +0100

    Indentation

--- src/cs/Lab2/WordCount/WordCountReducer.java
+++ src/cs/Lab2/WordCount/WordCountReducer.java
@@ -9,25 +9,25 @@ import java.util.Iterator;
 public class WordCountReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
 	private IntWritable totalWordCount = new IntWritable();
 	
-    // Overriding of the reduce function
+    	// Overriding of the reduce function
 	// Input : ([word, docId], List of subWordCounts)
 	// Output : ([word, docId], wordCount)
-    @Override
-    protected void reduce(final Text cleI, final Iterable<IntWritable> listevalI, final Context context) throws IOException,InterruptedException
-    {
-    	// Initiate the local variables
-        int sum = 0;
-        
-        // We sum all the sub wordCounts
-        Iterator<IntWritable> iterator = listevalI.iterator();
-        while (iterator.hasNext())
-        {
-        	sum += iterator.next().get();
-        }
-        
-        // Output
-        totalWordCount.set(sum);
-        context.write(cleI,totalWordCount);
-    }
+   	@Override
+    	protected void reduce(final Text cleI, final Iterable<IntWritable> listevalI, final Context context) throws IOException,InterruptedException
+    	{
+		// Initiate the local variables
+		int sum = 0;
+
+		// We sum all the sub wordCounts
+		Iterator<IntWritable> iterator = listevalI.iterator();
+		while (iterator.hasNext())
+		{
+			sum += iterator.next().get();
+		}
+
+		// Output
+		totalWordCount.set(sum);
+		context.write(cleI,totalWordCount);
+    	}
 
 }
