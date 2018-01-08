4a8173d67359bb3dfd0282a858155390eec050a8
Valentin GRASSET <33599781+pnmoins2@users.noreply.github.com>
2018-01-09 00:04:38 +0100

    Indentation

--- src/cs/Lab2/WordCountPerDoc/WordCountPerDocReducer.java
+++ src/cs/Lab2/WordCountPerDoc/WordCountPerDocReducer.java
@@ -18,67 +18,67 @@ public class WordCountPerDocReducer extends Reducer<Text,Text,Text,Text> {
 	private JSONObject wordCountAndWordsPerDocObject = new JSONObject();
 	private Text wordCountAndWordsPerDocText = new Text();
 	
-    // Overriding of the reduce function
+    	// Overriding of the reduce function
 	// Input : (docId, list of [word, wordCount])
 	// Output : ([word, docId], [wordCount, wordsPerDoc])
-    @SuppressWarnings("unchecked")
+   	@SuppressWarnings("unchecked")
 	@Override
-    protected void reduce(final Text cleI, final Iterable<Text> listevalI, final Context context) throws IOException,InterruptedException
-    {
-    	// Initiate the local variables
-        Long wordsPerDoc = new Long(0);
-        JSONObject wordAndWordCountObject;
-        String wordAndWordCountText;
-        String word;
-        Long wordCount;
-        List<JSONObject> cache = new ArrayList<JSONObject>();
-        
-        // For each word
-        Iterator<Text> iterator = listevalI.iterator();
-        while (iterator.hasNext())
-        {
-        	// Recover the JSON string
-        	wordAndWordCountText = iterator.next().toString();
-        	
-        	try {
-        		// Convert the JSON string to a JSON object
-        		wordAndWordCountObject = (JSONObject) this.parser.parse(wordAndWordCountText);
-        		
-        		// Add the JSON object to a list for the next step
-        		cache.add(wordAndWordCountObject);
-        		
-        		// Recover the wordCount related to the current word
-        		wordCount =  (Long) wordAndWordCountObject.get("wordCount");
-        		
-        		// Count the total number of word in the document
-        		wordsPerDoc += wordCount;
-        	} catch (ParseException e) {
-        		// TODO Auto-generated catch block
-        		e.printStackTrace();
-        	}
-        }
-        
-        // For each word
-        for(JSONObject valI : cache)
-        {
-        	// Recover the information within the JSON
-    		word = (String) valI.get("word");
-    		wordCount = (Long) valI.get("wordCount");
-    		
-    		// Output key
-    		wordAndDocIdObject.put("docId",cleI.toString());
-    		wordAndDocIdObject.put("word", word);
-    		
-    		// Output value
-    		wordCountAndWordsPerDocObject.put("wordCount", wordCount);
-    		wordCountAndWordsPerDocObject.put("wordsPerDoc", wordsPerDoc);
-    		
-    		// Convert the JSON objects to JSON strings
-    		wordAndDocIdText.set(wordAndDocIdObject.toJSONString());
-    		wordCountAndWordsPerDocText.set(wordCountAndWordsPerDocObject.toJSONString());
-    		
-    		// Output
-    		context.write(wordAndDocIdText,wordCountAndWordsPerDocText);
-        }
-    }
+    	protected void reduce(final Text cleI, final Iterable<Text> listevalI, final Context context) throws IOException,InterruptedException
+    	{
+		// Initiate the local variables
+		Long wordsPerDoc = new Long(0);
+		JSONObject wordAndWordCountObject;
+		String wordAndWordCountText;
+		String word;
+		Long wordCount;
+		List<JSONObject> cache = new ArrayList<JSONObject>();
+
+		// For each word
+		Iterator<Text> iterator = listevalI.iterator();
+		while (iterator.hasNext())
+		{
+			// Recover the JSON string
+			wordAndWordCountText = iterator.next().toString();
+
+			try {
+				// Convert the JSON string to a JSON object
+				wordAndWordCountObject = (JSONObject) this.parser.parse(wordAndWordCountText);
+
+				// Add the JSON object to a list for the next step
+				cache.add(wordAndWordCountObject);
+
+				// Recover the wordCount related to the current word
+				wordCount =  (Long) wordAndWordCountObject.get("wordCount");
+
+				// Count the total number of word in the document
+				wordsPerDoc += wordCount;
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		// For each word
+		for(JSONObject valI : cache)
+		{
+			// Recover the information within the JSON
+			word = (String) valI.get("word");
+			wordCount = (Long) valI.get("wordCount");
+
+			// Output key
+			wordAndDocIdObject.put("docId",cleI.toString());
+			wordAndDocIdObject.put("word", word);
+
+			// Output value
+			wordCountAndWordsPerDocObject.put("wordCount", wordCount);
+			wordCountAndWordsPerDocObject.put("wordsPerDoc", wordsPerDoc);
+
+			// Convert the JSON objects to JSON strings
+			wordAndDocIdText.set(wordAndDocIdObject.toJSONString());
+			wordCountAndWordsPerDocText.set(wordCountAndWordsPerDocObject.toJSONString());
+
+			// Output
+			context.write(wordAndDocIdText,wordCountAndWordsPerDocText);
+		}
+    	}
 }
