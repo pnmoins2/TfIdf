7260278ebf46f77da1e793bf10697646623c4dae
Valentin GRASSET <33599781+pnmoins2@users.noreply.github.com>
2018-01-09 00:13:32 +0100

    Indentation

--- src/cs/Lab2/Sort/SortReducer.java
+++ src/cs/Lab2/Sort/SortReducer.java
@@ -10,24 +10,24 @@ public class SortReducer extends Reducer<FloatWritable,Text,Text,FloatWritable> 
 	private Text docIdAndWord = new Text();
 	private FloatWritable tfIdf = new FloatWritable();
 	
-    // Overriding of the reduce function
+    	// Overriding of the reduce function
 	// Input : (-tfIdf, List of [word, docId])
 	// Output : ([word, docId], tfIdf)
 	@Override
-    protected void reduce(final FloatWritable cleI, final Iterable<Text> listevalI, final Context context) throws IOException,InterruptedException
-    {
-    	// Recover the tfIdf
-    	tfIdf.set(-cleI.get());
-    	
-        // For each [word, docId]
-        Iterator<Text> iterator = listevalI.iterator();
-        while (iterator.hasNext())
-        {
-        	// Recover the [docId, word]
-        	docIdAndWord.set(iterator.next().toString());
-        	
-        	// Output
-        	context.write(docIdAndWord, tfIdf);
-        }
-    }
+    	protected void reduce(final FloatWritable cleI, final Iterable<Text> listevalI, final Context context) throws IOException,InterruptedException
+    	{
+		// Recover the tfIdf
+		tfIdf.set(-cleI.get());
+
+		// For each [word, docId]
+		Iterator<Text> iterator = listevalI.iterator();
+		while (iterator.hasNext())
+		{
+			// Recover the [docId, word]
+			docIdAndWord.set(iterator.next().toString());
+
+			// Output
+			context.write(docIdAndWord, tfIdf);
+		}
+    	}
 }
